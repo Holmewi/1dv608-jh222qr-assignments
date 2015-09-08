7004a5c67b8609273faf14bbaa55fa9e624be4a2
Holmewi <Holmewi@Joakim-Holmewis-iMac.local>
2015-09-09 01:13:44 +0200

    Changed the timezone in DateTimeView.

--- 2-login/view/DateTimeView.php
+++ 2-login/view/DateTimeView.php
@@ -5,6 +5,7 @@ namespace view;
 class DateTimeView {
 
 	public function show() {
+		$timezone = date_default_timezone_set('Europe/Stockholm');
 		$timeString = date('l\, \t\h\e jS \of F Y\, \T\h\e \t\i\m\e \i\s H:i:s');
 
 		return '<p>' . $timeString . '</p>';
